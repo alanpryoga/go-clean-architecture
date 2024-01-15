b2c02205d5c1a504d63b47042f8629cb4c2c7ddd
Ade Syahlan Prayoga <ade.prayoga@erajaya.com>
2024-01-15 11:54:57 +0700

    Modify Redis cache instance visibility

--- internal/infrastructure/cache/cache_redis.go
+++ internal/infrastructure/cache/cache_redis.go
@@ -7,18 +7,18 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-type RedisCache struct {
-	db *redis.Client
+type redisCache struct {
+	client *redis.Client
 }
 
 func NewRedisCache(db *redis.Client) Cache {
-	return &RedisCache{
-		db: db,
+	return &redisCache{
+		client: db,
 	}
 }
 
-func (c *RedisCache) Get(ctx context.Context, key string) ([]byte, error) {
-	val, err := c.db.Get(ctx, key).Bytes()
+func (c *redisCache) Get(ctx context.Context, key string) ([]byte, error) {
+	val, err := c.client.Get(ctx, key).Bytes()
 	if err == redis.Nil {
 		return nil, ErrKeyNotFound
 	} else if err != nil {
@@ -27,19 +27,19 @@ func (c *RedisCache) Get(ctx context.Context, key string) ([]byte, error) {
 	return val, nil
 }
 
-func (c *RedisCache) Set(ctx context.Context, key string, value []byte) error {
-	return c.db.Set(ctx, key, value, 0).Err()
+func (c *redisCache) Set(ctx context.Context, key string, value []byte) error {
+	return c.client.Set(ctx, key, value, 0).Err()
 }
 
-func (c *RedisCache) SetWithTTL(ctx context.Context, key string, value []byte, ttl time.Duration) error {
-	return c.db.SetEx(ctx, key, value, ttl).Err()
+func (c *redisCache) SetWithTTL(ctx context.Context, key string, value []byte, ttl time.Duration) error {
+	return c.client.SetEx(ctx, key, value, ttl).Err()
 }
 
-func (c *RedisCache) Delete(ctx context.Context, key string) error {
-	return c.db.Del(ctx, key).Err()
+func (c *redisCache) Delete(ctx context.Context, key string) error {
+	return c.client.Del(ctx, key).Err()
 }
 
-func (c *RedisCache) Ping(ctx context.Context) error {
-	_, err := c.db.Ping(ctx).Result()
+func (c *redisCache) Ping(ctx context.Context) error {
+	_, err := c.client.Ping(ctx).Result()
 	return err
 }
